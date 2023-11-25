fe296b89916b92e92aea651ee141f5f0f52b01cf
Vladimir Iglovikov <iglovikov@gmail.com>
2023-11-24 17:54:11 -0800

    Fix

--- yolo5face/yoloface/utils/datasets.py
+++ yolo5face/yoloface/utils/datasets.py
@@ -2,8 +2,31 @@ import cv2
 import numpy as np
 
 
-def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scale_fill=False, scaleup=True):
-    # Resize image to a 32-pixel-multiple rectangle https://github.com/ultralytics/yolov3/issues/232
+def letterbox(
+    img: np.ndarray,
+    new_shape: int | tuple[int, int] = (640, 640),
+    color: tuple[int, int, int] = (114, 114, 114),
+    auto: bool = True,
+    scale_fill: bool = False,
+    scaleup: bool = True,
+) -> tuple[np.ndarray, tuple[float, float], tuple[float, float]]:
+    """
+    Resizes an image to a new shape while maintaining aspect ratio.
+
+    Args:
+        img: The original image.
+        new_shape: The new shape as a single int or a tuple of (height, width).
+        color: The color used for padding.
+        auto: Flag to apply minimum rectangle padding.
+        scale_fill: Flag to stretch image to fill new_shape.
+        scaleup: Flag to allow scaling up the image.
+
+    Returns:
+        A tuple containing:
+            - The resized image.
+            - The scaling ratio for width and height.
+            - The padding applied on width and height.
+    """
     shape = img.shape[:2]  # current shape [height, width]
     if isinstance(new_shape, int):
         new_shape = (new_shape, new_shape)
@@ -16,11 +39,11 @@ def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scale
     # Compute padding
     ratio = r, r  # width, height ratios
     new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
-    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    dw, dh = float(new_shape[1] - new_unpad[0]), float(new_shape[0] - new_unpad[1])  # wh padding
     if auto:  # minimum rectangle
         dw, dh = np.mod(dw, 64), np.mod(dh, 64)  # wh padding
     elif scale_fill:  # stretch
-        dw, dh = 0.0, 0.0
+        dw, dh = 0, 0
         new_unpad = (new_shape[1], new_shape[0])
         ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
 
